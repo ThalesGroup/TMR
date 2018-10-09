58f3e45d8990d064872a955a733275c070d3739d
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2018-10-09 14:29:02 +0200

    LICENCE, README and chiesel.sh

--- git.sh
+++ git.sh
@@ -1,20 +1,27 @@
 BASE_DIR=`dirname $0 | xargs readlink -f`
 
+# Init submodules
+git submodule update --init
+
+# submodules thales-risc-v-fault-detector
 cd $BASE_DIR/thales-risc-v-fault-detector
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
+# submodules thales-risc-v-jtag-router
 cd $BASE_DIR/thales-risc-v-jtag-router
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
+# submodules thales-risc-v-registers-router
 cd $BASE_DIR/thales-risc-v-registers-router
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
+# submodules thales-risc-v-chisel-project
 cd $BASE_DIR/thales-risc-v-chisel-project
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
