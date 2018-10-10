79e200dad2ad66082a5cb55fae68de17122c34f9
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2018-10-10 18:21:59 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,28 +1,27 @@
 BASE_DIR=`dirname $0 | xargs readlink -f`
 
-# Init submodules
+echo "### Init submodules ###"
 git submodule update --init
 
-# submodules thales-risc-v-fault-detector
+echo "### submodules thales-risc-v-fault-detector ###"
 cd $BASE_DIR/thales-risc-v-fault-detector
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
-# submodules thales-risc-v-jtag-router
+echo "### submodules thales-risc-v-jtag-router ###"
 cd $BASE_DIR/thales-risc-v-jtag-router
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
-# submodules thales-risc-v-registers-router
+echo "### submodules thales-risc-v-registers-router ###"
 cd $BASE_DIR/thales-risc-v-registers-router
 git submodule update --init rocket-chip
 cd rocket-chip
 git submodule update --init firrtl chisel3 torture hardfloat
 
-# submodules thales-risc-v-chisel-project
+echo "### submodules thales-risc-v-chisel-project ###"
 cd $BASE_DIR/thales-risc-v-chisel-project
 git submodule update --init testchipip
-cd rocket-chip
-git submodule update --init firrtl chisel3 torture hardfloat 
+git submodule update --init rocket-chip/firrtl rocket-chip/chisel3 rocket-chip/torture rocket-chip/hardfloat 
